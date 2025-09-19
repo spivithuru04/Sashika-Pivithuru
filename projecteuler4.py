e2fb3810410fbde5e1db0c3d8aa3b577a8a3ba85
spivithuru04 <spivithuru04@gmail.com>
2025-09-19 14:39:00 +0530

    edited projecteuler4.py

--- projecteuler4.py
+++ projecteuler4.py
@@ -1,4 +1,4 @@
-def find_largest_palindrome(a,b):
+def find_largest_palindrome(a,b):  ## function to find the largest palindrome
 
     largest_palindrome = 0
 
@@ -7,10 +7,10 @@ def find_largest_palindrome(a,b):
 
     for i in range(10**a):
         for j in range(i,10**b):
-            if i*j == int(str(i*j)[::-1]):
+            if i*j == int(str(i*j)[::-1]):  ## check the palindrome qaulities
                 if i*j > largest_palindrome:
-                    largest_palindrome = i*j
+                    largest_palindrome = i*j  ## save the largest palindrome
     
     return largest_palindrome
 
-print(find_largest_palindrome(3,3))
+print(find_largest_palindrome(3,3))  ## print the largest palindrome
