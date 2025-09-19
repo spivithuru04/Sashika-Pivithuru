10e724a205ef6f9216cac6c5336f4cf23a61ff96
spivithuru04 <spivithuru04@gmail.com>
2025-09-19 14:12:15 +0530

    edited projecteuler1 file

--- projecteuler1.py
+++ projecteuler1.py
@@ -1,12 +1,12 @@
-def multiple_sum(n):
+def multiple_sum(n):  ## create a function to get the sum of all the multiples of 3 or 5 below 1000
 
     total = 0
 
     for num in range(1,1000):
-        if num % 3 == 0 or num % 5 == 0:
-            total += num
+        if num % 3 == 0 or num % 5 == 0:  ## check , can num divide by 3 or 5
+            total += num  ## get the sum
     
-    return total
+    return total  ## get total from the function
 
-print(multiple_sum(1000))
+print(multiple_sum(1000))  ## print the sum
 
